67de84e1b9e0bbb88f53fcdc0b86d20871875a97
Frederico Santos <1119791+fredericosantos@users.noreply.github.com>
2021-04-03 02:52:16 +0100

    FAVICON UPDATE

--- project_2/chess_app.py
+++ project_2/chess_app.py
@@ -10,21 +10,27 @@ import ast
 
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+
 
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objs as go
+from chessboard import getChessboard, getHeatmap
+from styles import *
 
 # Read the .csv file with the preprocessed data.
-df = pd.read_csv("chess_app.csv", dtype={"pawns": int, "knights": int, "bishops": int,
-                                         "rooks": int, "queens": int},
-                 converters={"wKing_sqr": ast.literal_eval, "bKing_sqr": ast.literal_eval})
+df = pd.read_csv(
+    "chess_app.csv",
+    dtype={"pawns": int, "knights": int, "bishops": int, "rooks": int, "queens": int},
+    converters={"wKing_sqr": ast.literal_eval, "bKing_sqr": ast.literal_eval},
+)
 
 # Define function to output an 8*8 dataframe based on a vector of coordinates.
 
 
 def board_output(vector):
-
     brd = np.zeros((8, 8))
     for tup in vector:
         brd[tup] += 1
@@ -36,46 +42,70 @@ def board_output(vector):
 # board_output(wKing_sqr).to_csv("wKing_Heatmap.csv")
 df_king = board_output(df["wKing_sqr"])
 
+# FILLER STUFF ~ LET'S KEEP THIS FILE CLEAN, have other .py files with everything!
+x_coords = ["A", "B", "C", "D", "E", "F", "G", "H"]
+replacer = {i+1: x for i, x in enumerate(x_coords)}
+
+
 # Set stylesheets and app.
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.title = "Chess Analytics"
 
-# producing heatmap with imshow(should consider changing it with go)
-fig = px.imshow(df_king, color_continuous_scale=px.colors.sequential.Burgyl, labels=dict(
-    x="", y="", color="No of checkmates"),
-    x=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
-    y=['1', '2', '3', '4', '5', '6', '7', '8'][::-1])
+chessboard = getChessboard()
+
+
 
 # Defining app layout
-app.layout = html.Div([
-
-    html.Div([
-        html.H2("Chess App"),
-        html.Img(src="/assets/chess-app.jpg"),
-    ], className="banner"),
-
-    html.Div([
-        html.Div([
-            html.Label('Range Slider'),
-            dcc.RangeSlider(
-                id="rangeslider",
-                marks={i: 'label {}'.format(i) for i in range(0, 5)},
-                min=0,
-                max=5,
-                value=[0, 1],
-                step=1)
-        ], className="five columns"),
-        html.Div([
-            dcc.Graph(id="Victory Status", figure=fig)
-        ], className=" six columns"),
-
-    ], className="row"),
-])
+app.layout = html.Div(
+    style={"backgroundColor": colors["background"]},
+    children=[
+        html.Div(
+            [
+                html.H2("Chess App"),
+                html.Img(src="/assets/chess-app.jpg"),
+            ],
+            className="banner",
+        ),
+        html.Div(
+            [
+                html.Div(
+                    [
+                        html.Label("Range Slider"),
+                        dcc.RangeSlider(
+                            id="rangeslider",
+                            marks={i: "label {}".format(i) for i in range(0, 5)},
+                            min=0,
+                            max=5,
+                            value=[0, 1],
+                            step=1,
+                        ),
+                    ],
+                    className="five columns",
+                ),
+                html.Div(
+                    [
+                        dcc.Graph(
+                            id="Victory Status",
+                            figure=chessboard,
+                            config={
+                                "displayModeBar": False,
+                                "scrollZoom": False,
+                                "showAxisDragHandles": False,
+                            },
+                        )
+                    ],
+                    className=" six columns",
+                ),
+            ],
+            className="row",
+        ),
+    ],
+)
 # Statring the dash app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
-'''
+"""
 app.layout = html.Div([
     html.Label('Range Slider'),
     dcc.RangeSlider(
@@ -87,10 +117,10 @@ app.layout = html.Div([
         step=1
     ),
 ])
-'''
-'''
+"""
+"""
 fig = px.imshow(df_test, color_continuous_scale=px.colors.sequential.Burgyl, labels=dict(
     x="", y="", color="No of checkmates"),
     x=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'],
     y=['1', '2', '3', '4', '5', '6', '7', '8'])
-'''
+"""
---
+++ project_2/chessboard.py
@@ -0,0 +1,66 @@
+import plotly.graph_objects as go
+import pandas as pd
+
+
+def getChessboard(dimensions: int = 500, margin: int = 50):
+    row = [0, 1] * 4
+    boardmatrix = [row[::-1] if i % 2 == 1 else row for i in range(1, 9)]
+
+    chessboard = go.Figure(
+        layout=dict(
+            margin=dict(l=margin, r=margin, t=margin, b=margin, pad=10),
+            width=dimensions,
+            height=dimensions,
+            plot_bgcolor="rgba(0,0,0,0)",
+            paper_bgcolor="rgba(0,0,0,0)",
+            font_color="white",
+            coloraxis_showscale=False,
+            yaxis=dict(
+                range=[0.5, 8.5], color="white", tickfont_size=12, fixedrange=True
+            ),
+            xaxis=dict(
+                range=[-0.5, 7.5], color="white", tickfont_size=12, fixedrange=True
+            ),
+        )
+    )
+    chessboard.add_trace(
+        go.Heatmap(
+            x=["A", "B", "C", "D", "E", "F", "G", "H"],
+            y=list(range(1, 9)),
+            x0=0,
+            y0=0,
+            dx=0,
+            z=boardmatrix,
+            colorscale=["white", "black"],
+            showscale=False,
+        )
+    )
+
+    return chessboard
+
+
+def getHeatmap(chessboard: go.Figure, dataframe: pd.DataFrame):
+    """DataFrame must have columns named:
+    rows => 1 to 8
+    letters => A to H
+    freq => frequency"""
+    heatmap = go.Scatter(
+        x0=0,
+        y0=0,
+        dx=0,
+        x=dataframe.letters,
+        y=dataframe.rows,
+        name="Name B",
+        mode="markers",
+        opacity=1,
+        marker_symbol="square",
+        marker_line_color="#c12917",
+        marker_size=dataframe.freq,
+        marker_sizeref=4,
+        marker_sizemin=1,
+        marker_sizemode="area",
+        marker_opacity=1,
+        marker_color="#c12917",
+        hovertemplate="Test: %{y} - %{x}<br>number: 1<extra></extra>",
+    )
+    return heatmap
